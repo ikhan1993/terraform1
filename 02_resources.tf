7ebf7dac9d8ed9d2b390594e29d4e2e7fffec891
ikhan1993 <khan_1993_1@hotmail.co.uk>
2018-08-22 14:48:11 +0000

    variables replacing strings that may be repeated

--- 02_resources.tf
+++ 02_resources.tf
@@ -2,16 +2,16 @@
 # using google gcp service here
 # picking what type of machine and zon to use along with image of vm and nerworking required
 resource "google_compute_instance" "default" {
-	name = "test"
-	machine_type = "n1-standard-1"
-	zone = "europe-west2-c"
+	name = "${var.name}"
+	machine_type = "${var.machine_type}"
+	zone = "${var.zone}"
 	boot_disk {
 		initialize_params {
-			image = "centos-7"
+			image = "${var.image}"
 		}
 	}
 	network_interface {
-		network = "default"
+		network = "${var.network}"
 		access_config {
 			// Ephemeral IP...this being empty a ip will be assigned auto...comment
 		}
@@ -19,12 +19,12 @@ resource "google_compute_instance" "default" {
 	
 	// creating a way to ssh into the machine of user terraform
 	metadata {
-		sshKeys = "terraform:${file("~/.ssh/id_rsa.pub")}"
+		sshKeys = "${var.username}:${file("~/.ssh/id_rsa.pub")}"
 	}
 	provisioner "remote-exec" {
 		connection = {
-			type = "ssh"
-			user = "terraform"
+			type = "${var.ssh}"
+			user = "${var.username}"
 			private_key = "${file("~/.ssh/id_rsa")}"
 		}
 		// runs once vm created succesfully..executes 

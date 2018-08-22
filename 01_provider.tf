aff02518b5e621ffd93cdac545ea33180966e17a
israr <khan_1993_1@hotmail.co.uk>
2018-08-22 09:43:18 +0000

    update

--- 01_provider.tf
+++ 01_provider.tf
@@ -1,5 +1,6 @@
 # providing information for the gcp project 
 # credential private key provided from google
+# this file should be created before 'terraform init' is run
 provider "google" {
 	credentials = "${file("~/gcp.json")}"
 	project = "zeta-rush-204812"

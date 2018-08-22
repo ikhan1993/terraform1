318753d90e3ac8eaa53d7193b932b69678e664e6
ikhan1993 <khan_1993_1@hotmail.co.uk>
2018-08-22 10:40:27 +0000

    key updates

--- 01_provider.tf
+++ 01_provider.tf
@@ -2,7 +2,7 @@
 # credential private key provided from google
 # this file should be created before 'terraform init' is run
 provider "google" {
-	credentials = "${file("~/gcp.json")}"
-	project = "zeta-rush-204812"
+	credentials = "${file("/vagrant/gcpqa-214108-8f40765d258d.json")}"
+	project = "gcpqa-214108"
 	region = "europe-west2"
 }

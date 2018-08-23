031da2dd911a2368f138741bd0bb099cfec0a41c
vagrant <vagrant@localhost.localdomain>
2018-08-23 15:13:23 +0000

    update

--- 01_provider.tf
+++ 01_provider.tf
@@ -2,7 +2,7 @@
 # credential private key provided from google
 # this file should be created before 'terraform init' is run
 provider "google" {
-	credentials = "${file("/vagrant/gcpqa-214108-8f40765d258d.json")}"
-	project = "gcpqa-214108"
+	credentials = "${file("${var.path}")}"
+	project = "${var.project-id}"
 	region = "europe-west2"
 }
